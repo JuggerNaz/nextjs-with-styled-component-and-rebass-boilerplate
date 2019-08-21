38f6a6bf4fdab1bf36ee7152febbe850c9458d68
juggernaz <nazrul.muhaimin@gmail.com>
2019-08-21 17:58:36 +0800

    change(hero): add standard title and subtitle along with button on the hero section

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Box, Heading } from 'rebass'
+import { Flex, Box, Heading, Text } from 'rebass'
 import styled from 'styled-components'
 
 //local
@@ -9,14 +9,38 @@ import Navbar from '../core/Navbar'
 const CustomContainer = styled(Container)`
     background: rgb(134,37,205);
     background: linear-gradient(45deg, rgba(134,37,205,1) 0%, rgba(200,49,216,1) 100%);
-`;
+`
+
+const CTAButton = styled.a`
+    padding: 15px 0px;
+    width: 200px;
+    font-size: 18px;
+    border-radius: 5px;
+    background: red;
+    text-align: center;
+    margin: 15px 0px;
+`
 
 const Header = props =>
     <CustomContainer 
         bleed={true}
-        height={['200px','350px']}
+        height={['200px','300px']}
+        flexDirection='column'
     >
         <Navbar />
+        <Container
+            bleed={true}
+            justifyContent='center'
+        >
+            <Container
+                mt={4}
+                flexDirection='column'
+            >
+                <Text>Welcome to Salam App! </Text><br/>
+                <Heading>Let's 'bersalaman' to get closer!</Heading>
+                <CTAButton>Let's Jom!</CTAButton>
+            </Container>
+        </Container>
     </CustomContainer>
 
 export default Header
